a11f617a3227f1ef23c1ebc28423450824102469
nurali-code <nur3.dav.97@gmail.com>
2025-03-07 23:27:16 +0500

    most done

---
+++ send.php
@@ -0,0 +1,14 @@
+<?php
+
+$name = $_POST['name'];
+$contact = $_POST['tel'];
+$arr = array(
+	'👤 Имя: ' => $name,
+	'📞 Телефон: ' => $contact,
+);
+
+foreach($arr as $key => $value) {
+	$txt .= "<b>".$key."</b> ".$value."%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
